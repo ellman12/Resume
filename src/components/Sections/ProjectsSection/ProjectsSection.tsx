afaf1d2366627b30747921eac8914cb602dee8f7
Elliott DuCharme <ellduc4@gmail.com>
2026-01-30 16:23:31 -0600

    Swap project positions and update WTB's description

--- src/components/Sections/ProjectsSection/ProjectsSection.tsx
+++ src/components/Sections/ProjectsSection/ProjectsSection.tsx
@@ -5,18 +5,17 @@ export default function ProjectsSection() {
     return (
         <Section title="Projects">
             <div className="flex flex-col gap-4">
+                <Project name="WingTech Bot Rewrite" href="https://github.com/ellman12/WingTechBot-MK3" hrefDisplay="github.com/ellman12/WingTechBot">
+                    <li>Led a full rewrite of a Discord bot, migrating from C# to TypeScript using Discord.js improving API compatibility and long-term maintainability.</li>
+                    <li>Designed a modular command architecture with analytics, moderation tools, and database storage.</li>
+                    <li>Integrated an LLM-powered chatbot using Google Gemini.</li>
+                    <li>Automated builds, testing, and production deployments using CI/CD pipelines to support rapid iteration.</li>
+                </Project>
+
                 <Project name="MemoryMosaic" href="https://github.com/ellman12/MemoryMosaic" hrefDisplay="github.com/ellman12/MemoryMosaic">
                     <li>A Blazor app for storing a lifetime of photos and videos in PostgreSQL.</li>
                     <li>Gained extensive experience with media metadata and creating/optimizing web apps and databases.</li>
                     <li>Libraries can scale into the millions with no performance issues.</li>
-                </Project>
-
-                <Project name="WingTech Bot Rewrite" href="https://github.com/ellman12/WingTechBot-MK3" hrefDisplay="github.com/ellman12/WingTechBot">
-                    <li>Rewrote and modernized a C# Discord bot used by a community of 40+ members.</li>
-                    <li>Deployed with Docker on AWS EC2, integrating RDS for scalable, persistent storage.</li>
-                    <li>Added reaction tracking, karma, games, and inside jokes.</li>
-                    <li>Features LLM-powered AI chatbot using Google Gemini.</li>
-                    <li>Practiced test-driven development using NUnit with comprehensive unit and integration tests.</li>
                 </Project>
             </div>
         </Section>
