eb715822cc174ec724406a5dbc32148449236abe
Elliott DuCharme <ellduc4@gmail.com>
2026-07-09 16:05:08 -0500

    Drop soft skills and tweak skills list

--- src/components/Sections/SkillsSection/SkillsSection.tsx
+++ src/components/Sections/SkillsSection/SkillsSection.tsx
@@ -1,49 +1,15 @@
 import Section from "../../../Section.tsx";
-import SkillTooltip from "./SkillTooltip.tsx";
-import {Fragment, ReactNode} from "react";
-
-type Skill = {
-    name: string;
-    tooltip: string | ReactNode;
-}
-
-const softSkills: Skill[] = [
-    {
-        name: "Adaptable",
-        tooltip: "Throughout my years of experience, I've excelled at picking up new technologies and adapting to new teams and work."
-    },
-    {
-        name: "Innovative",
-        tooltip: "I have a keen eye for finding more efficient methods of doing things, and I enjoy improving the efficiency of code and other developers."
-    },
-    {
-        name: "Self-motivated",
-        tooltip: "I thrive on setting goals and taking the initiative to complete them without external pressure. My constant drive to learn and improve keeps me engaged and focused on achieving success."
-    },
-    {
-        name: "Tenacious",
-        tooltip: "When issues arise during development, I always find a way to overcome them, either by adjusting my approach or creating a new one."
-    }
-];
 
 export default function SkillsSection() {
     return (
         <Section title="Skills">
             <div className="flex flex-col gap-1">
                 <p>
-                    <b>Languages:</b> TypeScript, C#, SQL, Java, Python, C++
+                    <b>Languages:</b> TypeScript, C#, Java, Python, C++
                 </p>
 
                 <p>
-                    <b>Technologies:</b> React, .NET, Vite, Next.js, Vitest, Godot
-                </p>
-
-                <p>
-                    <b>Soft Skills:</b>&nbsp;
-                    {softSkills.map((skill, index) => (<Fragment key={skill.name}>
-                        <SkillTooltip key={skill.name} skill={skill.name} tooltip={skill.tooltip}/>
-                        {index < 3 && <>,&nbsp;</>}
-                    </Fragment>))}
+                    <b>Technologies:</b> React, .NET, PostgreSQL, Next.js, Node.js, Docker, Kysely, GDAL, Tailwind
                 </p>
             </div>
         </Section>
